98739e72aa2c3aabecb3e75937d3ecf8e6d4f7b8
Elias Klein <klein.elias@t-online.de>
2025-08-09 19:27:41 +0200

    refactor parry

--- scripts/fsm/states/parry.gd
+++ scripts/fsm/states/parry.gd
@@ -1,18 +1,27 @@
 extends State
 
-@export
-var is_in_parry_window: bool = false
+var is_in_parry_window: bool
 
 func enter() -> void:
 	super()
+	is_in_parry_window = true
 	player.anim_sprite.play("parry")
+	player.anim_sprite.frame_changed.connect(parry_over)
 	print($'../..'.name + " " + self.name)
+
+func exit() -> void:
+	is_in_parry_window = false
+	player.anim_sprite.frame_changed.disconnect(parry_over)
 
 func update(delta: float) -> State:
 	if anim_fin:
 		return idle_state
 	return null
 
+func parry_over() -> void:
+	if player.anim_sprite.animation == 'parry':
+		if player.anim_sprite.frame == 2:
+			is_in_parry_window = false
+
 func is_parrying() -> bool:
-	# add correct parry logic
 	return is_in_parry_window
--- scripts/player_animsprite.gd
+++ scripts/player_animsprite.gd
@@ -9,15 +9,8 @@ func _on_frame_changed() -> void:
 			match frame:
 				2:
 					play_sword_woosh.emit()
-		'parry':
-			match frame:
-				var x when x > 2:
-					print($'..'.name, " in parry animation aframe ", frame)
-					$'../state_machine/parry'.is_in_parry_window = false
 
 func _on_animation_changed() -> void:
 	match animation:
 		'fail', 'death':
 			play_hit.emit()
-		'parry':
-			$'../state_machine/parry'.is_in_parry_window = true
