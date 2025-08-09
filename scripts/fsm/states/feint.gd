9d1508e0c0a64268c73b9013c48aa9c020adbfff
Andrea Lange <andrea.p.lange@gmail.com>
2025-08-09 13:34:46 +0200

    add return state and animation wait in feint.gd

--- scripts/fsm/states/feint.gd
+++ scripts/fsm/states/feint.gd
@@ -2,11 +2,14 @@ extends State
 
 @onready
 var _animatedSprite = get_node("../../AnimatedSprite2D")
+@onready
+var idle_state: State = $idle
 
 func enter() -> void:
 	_animatedSprite.play("feint")
+	await _animatedSprite.animation_finished
 	print("player1 feint")
 
 func update(delta: float) -> State:
 	# return to idle after feint time
-	return null
+	return idle_state
