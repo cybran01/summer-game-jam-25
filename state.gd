42f08bfd56807232aa9c4ae7de00f47db562b715
Elias Klein <klein.elias@t-online.de>
2025-08-09 15:23:57 +0200

    add everything for player 2

---
+++ state.gd
@@ -0,0 +1,24 @@
+class_name State
+extends Node
+
+var player: Player
+
+var anim_fin: bool
+
+@onready
+var idle_state: State = $'../idle'
+@onready
+var attack_state: State = $'../attack'
+@onready
+var feint_state: State = $'../feint'
+@onready
+var parry_state: State = $'../parry'
+
+func enter() -> void:
+	anim_fin = false
+
+func update(delta: float) -> State:
+	return null
+
+func is_parrying() -> bool:
+	return false
